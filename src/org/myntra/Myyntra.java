80c76c2c0a954d8d9f5986c98c6cf72112c8d687
jenimike <jeniraj16@gmail.com>
2025-05-25 15:47:45 +0530

    commited

---
+++ src/org/myntra/Myyntra.java
@@ -0,0 +1,21 @@
+package org.myntra;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Myyntra {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.myntra.com/register");
+		WebElement sm= driver.findElement(By.xpath("//input[@autocomplete='new-password']"));
+		sm.sendKeys("9791125364");
+		WebElement smm= driver.findElement(By.xpath("//input[@type='checkbox']"));
+		smm.click();
+		WebElement smmd= driver.findElement(By.xpath("//div[@class='submitBottomOption']"));
+		smmd.click();
+	}
+
+}
