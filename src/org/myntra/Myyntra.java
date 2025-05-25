d5b495b4380adfbc1c22d44cb7d7aac7f67107f8
jenimike <jeniraj16@gmail.com>
2025-05-25 16:28:37 +0530

    code modified

--- src/org/myntra/Myyntra.java
+++ src/org/myntra/Myyntra.java
@@ -11,11 +11,12 @@ public class Myyntra {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.myntra.com/register");
 		WebElement sm= driver.findElement(By.xpath("//input[@autocomplete='new-password']"));
-		sm.sendKeys("9791125364");
+		sm.sendKeys("9791152364");
 		WebElement smm= driver.findElement(By.xpath("//input[@type='checkbox']"));
 		smm.click();
 		WebElement smmd= driver.findElement(By.xpath("//div[@class='submitBottomOption']"));
 		smmd.click();
+		
 	}
 
 }
